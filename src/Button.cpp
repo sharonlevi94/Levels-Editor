936744f8c21fcfa6ba268fd397609594fe21687b
Sharon Levi <73107447+sharonlevi94@users.noreply.github.com>
2020-12-17 16:38:51 +0200

    Update Button.cpp

--- src/Button.cpp
+++ src/Button.cpp
@@ -1,15 +1,18 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-class Button
-{
-public:
-	Button(sf::Vector2f size,sf::Vector2f position,sf::Texture texture);
-	void drawButton(sf::RenderWindow& window);
+#include "Button.h"
+Button::Button(sf::Vector2f size, sf::Vector2f position, sf::Texture texture) {
+	m_size = size;
+	m_position = position;
+	m_texture = texture;
 
-private:
-	sf::Vector2f m_size;
-	sf::Vector2f m_position;
-	sf::Texture m_texture;
-	
-};
+}
 
+void Button::drawButton(sf::RenderWindow& window) {
+	sf::RectangleShape rec(m_size);
+	rec.setTexture(&m_texture);
+	rec.setPosition(m_position);
+	if (this->m_size.x == 100) {
+		rec.setOutlineColor(sf::Color::White);
+		rec.setOutlineThickness(3);
+	}
+	window.draw(rec);
+}
